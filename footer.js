421c35f12a121240b2242faba54b194233c656ca
Aravindh Mahendran <aravindh.2138@gmail.com>
2020-12-29 17:07:58 +0100

    Footer links open in new tab

--- footer.js
+++ footer.js
@@ -2,8 +2,8 @@ document.write('\
 <div id="footer"> \
 <hr> \
 <div class="left"> \
-<a href="http://research.google/people/AravindhMahendran">Google Research Profile</a> \
-| <a href="https://scholar.google.co.uk/citations?user=lAjGbLMAAAAJ">Google Scholar Profile</a> \
-| <a href="https://www.linkedin.com/in/aravindh-mahendran-09001653/">LinkedIn</a> </div> \
+<a href="http://research.google/people/AravindhMahendran" target="_blank" rel="noopener noreferrer">Google Research Profile</a> \
+| <a href="https://scholar.google.co.uk/citations?user=lAjGbLMAAAAJ" target="_blank" rel="noopener noreferrer">Google Scholar Profile</a> \
+| <a href="https://www.linkedin.com/in/aravindh-mahendran-09001653/" target="_blank" rel="noopener noreferrer">LinkedIn</a> </div> \
 <div class="right"> Page last updated on December 29, 2020 (GMT) \
 </div> <!-- end footer --> ');

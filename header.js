de1b881c3e61690ea896a97b9c546bf60fb49c1e
Aravindh Mahendran <aravindh.2138@gmail.com>
2020-12-29 17:20:06 +0100

    Add new photo to header

--- header.js
+++ header.js
@@ -3,5 +3,5 @@ document.write('\
 <h1><a href="index.html">Aravindh Mahendran</a></h1> \
 <h2>Research Scientist, Google Research, Brain Team</h2> <br> \
 </div> \
-<div style="float:right"><img src="figures/me_scotland_face.jpg" height=100px style="PADDING-TOP: 10px;PADDING-RIGHT: 10px" /></div> \
+<div style="float:right"><img src="figures/me_in_office_v3_facecrop.jpg" height=100px style="PADDING-TOP: 10px;PADDING-RIGHT: 10px" /></div> \
 </div><!-- end header -->');

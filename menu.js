5d3d5e3d4f8c35749a551a6c3362f7f1cb2c3b2d
DESKTOP-U0QP6NP\aravi <aravindh.2138@gmail.com>
2020-12-29 17:32:31 +0100

    Updates to menu
    Publication link now points to google scholar.
    Resume marked as outdated.

--- menu.js
+++ menu.js
@@ -2,9 +2,9 @@ document.write('\
 <!-- begin right division --> \
 <div id="right"> <p class="spaced"> \
 <a href="index.html">Home</a> <br/> \
-<a href="publications.html">Publications</a> <br/> \
+<a href="https://scholar.google.co.uk/citations?user=lAjGbLMAAAAJ" target="_blank" rel="noopener noreferrer">Publications</a> <br/> \
 <a href="education.html">Education</a> <br/> \
-<a href="resume.pdf" target=_blank>Resume [pdf]</a> <br /> \
+<a href="resume.pdf" target=_blank>Resume [pdf - outdated]</a> <br /> \
 <a href="life.html">Life</a> <br/> \
 </p> </div> \
 <!-- end right division -->');

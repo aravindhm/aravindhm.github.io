92b74eb3b9a7205a31c001024e938609ba2dbdcf
Aravindh Mahendran <aravindh.2138@gmail.com>
2020-12-29 17:04:04 +0100

    More footer links

--- footer.js
+++ footer.js
@@ -2,6 +2,8 @@ document.write('\
 <div id="footer"> \
 <hr> \
 <div class="left"> \
-<a href="http://research.google/people/AravindhMahendran/>Google Research Profile</a> </div> \
+<a href="http://research.google/people/AravindhMahendran">Google Research Profile</a> \
+| <a href="https://scholar.google.co.uk/citations?user=lAjGbLMAAAAJ">Google Scholar Profile</a> \
+| <a href="https://www.linkedin.com/in/aravindh-mahendran-09001653/">LinkedIn</a> </div> \
 <div class="right"> Page last updated on December 29, 2020 (GMT) \
 </div> <!-- end footer --> ');

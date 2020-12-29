94ade25cda8bed967477181c2940731a1e7df004
DESKTOP-U0QP6NP\aravi <aravindh.2138@gmail.com>
2020-12-29 18:14:27 +0100

    New resume. Still needs a proofread.

--- menu.js
+++ menu.js
@@ -4,7 +4,7 @@ document.write('\
 <a href="index.html">Home</a> <br/> \
 <a href="https://scholar.google.co.uk/citations?user=lAjGbLMAAAAJ" target="_blank" rel="noopener noreferrer">Publications</a> <br/> \
 <a href="education.html">Education</a> <br/> \
-<a href="resume.pdf" target=_blank>Resume [pdf - outdated]</a> <br /> \
+<a href="resume.pdf" target=_blank>Resume [pdf]</a> <br /> \
 <a href="life.html">Life</a> <br/> \
 </p> </div> \
 <!-- end right division -->');

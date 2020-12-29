d01045de724584d066058643a46febbeb0804c34
Aravindh Mahendran <aravindh.2138@gmail.com>
2020-12-29 16:02:28 +0100

    Update link in footer

--- footer.js
+++ footer.js
@@ -2,7 +2,6 @@ document.write('\
 <div id="footer"> \
 <hr> \
 <div class="left"> \
-<a href="http://www.ox.ac.uk/" target="_blank">University of Oxford</a> \
-| <a href="http://www.robots.ox.ac.uk/~vgg/" target="_blank">Visual Geometry Group</a> </div> \
-<div class="right"> Page last updated on February 07, 2015 (GMT) \
+<a href="http://research.google/people/AravindhMahendran/>Google Research Profile</a> </div> \
+<div class="right"> Page last updated on December 29, 2020 (GMT) \
 </div> <!-- end footer --> ');
